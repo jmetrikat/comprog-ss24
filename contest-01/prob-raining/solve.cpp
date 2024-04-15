c8d468da21d5f282ef09e429b8fd8eb995e3d474
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-04-15 09:24:42 +0200

    Refactor variable names for raining problem

--- contest-01/prob-raining/solve.cpp
+++ contest-01/prob-raining/solve.cpp
@@ -31,26 +31,26 @@ int main() {
         }
 
         // populate left vector with max heights
-        vector<int> left(heights.size(), 0);
+        vector<int> trapped_from_left(heights.size(), 0);
         int max_height = heights[0];
 
         for (int i = 1; i < heights.size() - 1; i++) {
             max_height = max(max_height, heights[i]);
-            left[i] = max_height - heights[i];
+            trapped_from_left[i] = max_height - heights[i];
         }
 
-        // populate left vector with max heights
-        vector<int> right(heights.size(), 0);
+        // populate right vector with max heights
+        vector<int> trapped_from_(heights.size(), 0);
         max_height = heights[heights.size() - 1];
 
         for (int i = heights.size() - 2; i > 0; i--) {
             max_height = max(max_height, heights[i]);
-            right[i] = max_height - heights[i];
+            trapped_from_[i] = max_height - heights[i];
         }
 
         int trapped = 0;
         for (int i = 0; i < heights.size(); i++) {
-            trapped += min(left[i], right[i]);
+            trapped += min(trapped_from_left[i], trapped_from_[i]);
         }
 
         cout << trapped << endl;
