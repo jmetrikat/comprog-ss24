375896b90304f133ffda1bb54bbe08a988b85a3c
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-04-28 16:00:52 +0200

    Add contest-03 problems and samples

---
+++ contest-03/prob-hedge/solve.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int testcases;
+    cin >> testcases;
+
+    while (testcases--) {
+        // content
+    }
+
+    return 0;
+}
