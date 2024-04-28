8e7b7eca9dc9f40ec283462cbf83c1c0ce6c8081
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-04-28 15:59:47 +0200

    Add sample automation to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ Expected folder structure:
         |- prob-yy.pdf
         |- prob-yy.pdf
         |- …
+        |- samples-yy
+            |- 1.in
+            |- 1.ans
+            |- …
+        |- samples-yy
+            |- …
     |- contest-xx
     |- …
 
@@ -22,6 +28,7 @@ import os
 import shutil
 
 problems = [f for f in os.listdir('.') if f.startswith('prob-')]
+samples = [f for f in os.listdir('.') if f.startswith('samples-')]
 
 for problem in problems:
     dirname = problem.replace('.pdf', '')
@@ -29,6 +36,13 @@ for problem in problems:
     shutil.move(problem, f"{dirname}/{problem}")
     shutil.copy('../templates/solve.cpp', f"{dirname}/solve.cpp")
     os.makedirs(f"{dirname}/samples", exist_ok=True)
+
+for sample in samples:
+    problem = sample.replace('samples-', '')
+    samplefiles = [f for f in os.listdir(f'./samples-{problem}') if f.endswith('.in') or f.endswith('.ans')]
+    for samplefile in samplefiles:
+        shutil.move(f'samples-{problem}/{samplefile}', f'prob-{problem}/samples/{samplefile}')
+    shutil.rmtree(f'samples-{problem}')
 
 os.symlink('../templates/Makefile', 'Makefile')
 os.symlink('../templates/validator.py', 'validator.py')
