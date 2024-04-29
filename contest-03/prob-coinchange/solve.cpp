db53df2e7f1f0fdde394d8fbdd3599d3cb4ab0c2
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-04-29 23:39:11 +0200

    Add coinchange solution

--- contest-03/prob-coinchange/solve.cpp
+++ contest-03/prob-coinchange/solve.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 
-#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define rep(a, b) for (ll a = 0; a < (b); ++a)
 #define all(a) (a).begin(), (a).end()
 #define endl '\n'
+#define ll long long
 
 using namespace std;
 
@@ -11,11 +12,44 @@ int main() {
     cin.tie(nullptr);
     cout.precision(10);
 
-    int testcases;
+    ll testcases;
     cin >> testcases;
 
     while (testcases--) {
-        // content
+        ll debt, numberOfCoins;
+        cin >> debt >> numberOfCoins;
+
+        vector<ll> coins(numberOfCoins);
+        for (auto& e : coins) cin >> e;
+
+        ll maxSize = debt + 1.5 * pow(10, 6);
+        vector<ll> dp(maxSize + 1, LONG_LONG_MAX);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        // classical upwards coin change dp calculation
+        for (ll i = 2; i <= maxSize; ++i) {
+            rep(j, numberOfCoins) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // downwards coin change dp calculation to consider solution with change
+        for (ll i = maxSize; i > 1; --i) {
+            rep(j, numberOfCoins) {
+                if (coins[j] <= i) {
+                    dp[i - coins[j]] = min(dp[i - coins[j]], dp[i] + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        cout << dp[debt] << endl;
     }
 
     return 0;
