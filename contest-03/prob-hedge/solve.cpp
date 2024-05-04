1ea3d9db4875eca986cae3fcbea5c5b2344c2a3e
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-04 15:02:20 +0200

    feat: add hedge solution

--- contest-03/prob-hedge/solve.cpp
+++ contest-03/prob-hedge/solve.cpp
@@ -5,6 +5,9 @@
 #define endl '\n'
 #define ll long long
 
+#define Graph vector<vector<ll>>
+#define MOD 998244353
+
 using namespace std;
 
 int main() {
@@ -15,8 +18,21 @@ int main() {
     ll testcases;
     cin >> testcases;
 
+    Graph dp(2001, vector<ll>(2001, 0));
+    dp[0][0] = 1;
+
+    // precompute the number of ways to arrange i planks into j sections
+    for (ll i = 1; i <= 2000; i++) {
+        for (ll j = 1; j <= i; j++) {
+            // either add the plan as new section and rearrange or add the plank to an existing section
+            dp[i][j] = ((i - j + 1) * dp[i - 1][j - 1] + (j * dp[i - 1][j])) % MOD;
+        }
+    }
+
     while (testcases--) {
-        // content
+        ll numberOfPlanks, amountOfSections;
+        cin >> numberOfPlanks >> amountOfSections;
+        cout << dp[numberOfPlanks][amountOfSections] << endl;
     }
 
     return 0;
