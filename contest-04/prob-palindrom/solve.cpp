48d1c57b11f060cfbdcc2184fab5161f10f7aa87
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-09 00:31:59 +0200

    refactor: palindrome comments and dp

--- contest-04/prob-palindrom/solve.cpp
+++ contest-04/prob-palindrom/solve.cpp
@@ -24,17 +24,17 @@ int main() {
         for (ll i = 0; i <= n - len; ++i) {
             ll j = i + len - 1;
             if (s[i] == s[j]) {
-                // outer characters are equal
-                // base case 'aa' has costs of 0, as this is alread a palindrom
+                // 'aa' has costs of 0, as this is alread a palindrom
                 // 'a…a' has cost of '…', as only the inner part needs to be turned into a palindrom
-                dp[i][j] = len == 2 ? 0 : dp[i + 1][j - 1];
+                dp[i][j] = dp[i + 1][j - 1];
             } else {
-                // outer characters are different: 3 possible ways to turn the substring into a palindrom
-                // 1. remove the first character: ab…b -> b…b
-                // 2. remove the last character: b…ba -> b…b
-                // 3. replace one of the characters with the other: a…b -> a…a or b…b
+                // 3 possible ways to turn the substring into a palindrom
                 // we never add a character as it's equal to removing one: ab…b -> ab…ba = b…b
-                dp[i][j] = min(min(dp[i + 1][j] + 1, dp[i][j - 1] + 1), dp[i + 1][j - 1] + 1);
+                dp[i][j] = 1 + min({
+                                   dp[i + 1][j],     // remove the first character: ab…b -> b…b
+                                   dp[i][j - 1],     // remove the last character: b…ba -> b…b
+                                   dp[i + 1][j - 1]  // replace one of the characters with the other: a…b -> a…a or b…b
+                               });
             }
         }
     }
