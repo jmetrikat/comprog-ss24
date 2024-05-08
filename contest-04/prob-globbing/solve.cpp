0a540547c6ea57b9aa4eb8e8ae36f8dea41f2899
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-08 16:37:06 +0200

    feat: add globbing and ip-lookup solution

--- contest-04/prob-globbing/solve.cpp
+++ contest-04/prob-globbing/solve.cpp
@@ -2,10 +2,49 @@
 
 #define rep(a, b) for (ll a = 0; a < (b); ++a)
 #define all(a) (a).begin(), (a).end()
-#define endl '\n'
 #define ll long long
 
 using namespace std;
+
+bool machtes(const string &pattern, const string &text) {
+    size_t textLength = text.size();
+
+    // pattern contains the structure <prefix>*<suffix>, * means 0 or more characters
+    if (pattern.find('*') != string::npos) {
+        string prefix = pattern.substr(0, pattern.find('*'));
+        string suffix = pattern.substr(pattern.find('*') + 1);
+
+        // check that text is not shorter than prefix and suffix
+        if (suffix.size() + prefix.size() > textLength) return false;
+
+        // prefix and suffix need to match the text
+        if (text.substr(0, prefix.size()) != prefix || text.substr(textLength - suffix.size()) != suffix) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // pattern contains the structure <prefix>+<suffix>, + means at least one character
+    if (pattern.find('+') != string::npos) {
+        string prefix = pattern.substr(0, pattern.find('+'));
+        string suffix = pattern.substr(pattern.find('+') + 1);
+
+        // check there is at least one character between prefix and suffix
+        if (suffix.length() + prefix.size() + 1 > textLength) return false;
+
+        // prefix and suffix need to match the text
+        if (text.substr(0, prefix.size()) != prefix || text.substr(textLength - suffix.size()) != suffix) {
+            return false;
+        }
+
+        // check if at least one character is between prefix and suffix
+        return (textLength > prefix.size() + suffix.size());
+    }
+
+    // no special characters in the pattern
+    return pattern == text;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -14,9 +53,18 @@ int main() {
 
     ll testcases;
     cin >> testcases;
+    cin.ignore();
 
     while (testcases--) {
-        // content
+        string pattern, text;
+        getline(cin, pattern);
+        getline(cin, text);
+
+        if (machtes(pattern, text)) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
     }
 
     return 0;
--- contest-04/prob-ip-lookup/solve.cpp
+++ contest-04/prob-ip-lookup/solve.cpp
@@ -7,17 +7,75 @@
 
 using namespace std;
 
+struct TrieNode {
+    TrieNode *children[2] = {nullptr};
+    bool isSubnet = false;
+
+    TrieNode() = default;
+
+    ~TrieNode() {
+        delete children[0];
+        delete children[1];
+    }
+};
+
+// insert IP or subnet into trie
+void insert(TrieNode *root, const string &ip) {
+    TrieNode *current = root;
+    for (char c : ip) {
+        if (c == '*') {
+            current->isSubnet = true;
+            return;
+        }
+        ll idx = c - '0';
+        if (current->children[idx] == nullptr) {
+            current->children[idx] = new TrieNode();
+        }
+        current = current->children[idx];
+    }
+}
+
+// lookup IP in trie
+bool lookup(TrieNode *root, const string &ip) {
+    TrieNode *current = root;
+    for (char c : ip) {
+        if (current->isSubnet) {
+            return true;
+        }
+        ll idx = c - '0';
+        if (current->children[idx] == nullptr) {
+            return false;
+        }
+        current = current->children[idx];
+    }
+    return true;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    ll testcases;
-    cin >> testcases;
+    ll noOfActiveIPs, noOfEmployeIPs;
+    cin >> noOfActiveIPs;
 
-    while (testcases--) {
-        // content
+    string ip;
+    TrieNode *root = new TrieNode();
+
+    // insert all active IPs into trie
+    for (ll i = 0; i < noOfActiveIPs; ++i) {
+        cin >> ip;
+        insert(root, ip);
     }
 
+    cin >> noOfEmployeIPs;
+
+    // lookup all employee IPs in trie
+    for (ll i = 0; i < noOfEmployeIPs; ++i) {
+        cin >> ip;
+        cout << (lookup(root, ip) ? "Yes" : "No") << endl;
+    }
+
+    delete root;
     return 0;
 }
