0f76a6914e70a508207e2cc91a8be106acb61243
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-20 19:01:01 +0200

    feat: add shoal solution

--- contest-05/prob-shoal/solve.cpp
+++ contest-05/prob-shoal/solve.cpp
@@ -4,19 +4,58 @@
 #define all(a) (a).begin(), (a).end()
 #define endl '\n'
 #define ll long long
+#define Graph vector<vector<ll>>
 
 using namespace std;
+
+void dfs(ll node, Graph& adj, vector<bool>& visited) {
+    stack<ll> s;
+    s.push(node);
+    while (!s.empty()) {
+        ll v = s.top();
+        s.pop();
+        if (!visited[v]) {
+            visited[v] = true;
+            for (ll u : adj[v]) {
+                if (!visited[u]) {
+                    s.push(u);
+                }
+            }
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    ll testcases;
-    cin >> testcases;
+    ll numberOfPuddles, numberOfMoats;
+    cin >> numberOfPuddles >> numberOfMoats;
 
-    while (testcases--) {
-        // content
+    Graph adj(numberOfPuddles);
+    while (numberOfMoats--) {
+        ll a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    vector<bool> visited(numberOfPuddles, false);
+    dfs(0, adj, visited);
+
+    bool allConnected = true;
+    rep(i, numberOfPuddles) {
+        if (!visited[i]) {
+            allConnected = false;
+            break;
+        }
+    }
+
+    if (allConnected) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
     }
 
     return 0;
