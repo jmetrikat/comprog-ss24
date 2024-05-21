7bc6aa4618ecbb1614271709cf1e7e27719d2240
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-21 22:31:09 +0200

    feat: add topological sort solution for indiana

--- contest-05/prob-indiana/solve.cpp
+++ contest-05/prob-indiana/solve.cpp
@@ -4,8 +4,63 @@
 #define all(a) (a).begin(), (a).end()
 #define endl '\n'
 #define ll long long
+#define Graph vector<vector<ll>>
 
 using namespace std;
+
+void solve(ll n, ll h, vector<pair<ll, ll>> &hints) {
+    Graph adj(n + 1);
+    vector<ll> in_degree(n + 1, 0);
+
+    // build graph with directed edges based on hints
+    for (auto &hint : hints) {
+        ll a = hint.first;
+        ll b = hint.second;
+
+        adj[a].push_back(b);
+        in_degree[b]++;
+    }
+
+    // find nodes with in-degree 0
+    queue<ll> q;
+    for (ll i = 1; i <= n; ++i) {
+        if (in_degree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    // topological sort
+    vector<ll> topological_order;
+    bool multiple_sequences = false;
+
+    while (!q.empty()) {
+        if (q.size() > 1) {
+            multiple_sequences = true;
+        }
+        ll node = q.front();
+        q.pop();
+        topological_order.push_back(node);
+
+        for (ll neighbor : adj[node]) {
+            if (--in_degree[neighbor] == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+
+    // check if topological sort is valid
+    if ((ll)topological_order.size() != n) {
+        cout << "recheck hints" << endl;
+    } else if (multiple_sequences) {
+        cout << "missing hints" << endl;
+    } else {
+        for (ll i = 0; i < (ll)topological_order.size(); ++i) {
+            if (i > 0) cout << " ";
+            cout << topological_order[i];
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,7 +71,11 @@ int main() {
     cin >> testcases;
 
     while (testcases--) {
-        // content
+        ll n, h;
+        cin >> n >> h;
+        vector<pair<ll, ll>> hints(h);
+        for (auto &[a, b] : hints) cin >> a >> b;
+        solve(n, h, hints);
     }
 
     return 0;
