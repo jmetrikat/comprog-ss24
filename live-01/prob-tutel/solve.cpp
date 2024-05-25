062fe6fd85ee0764bbd294f0053c36b1b454889b
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-05-25 12:15:56 +0200

    feat: add live-01 contest problems and samples

---
+++ live-01/prob-tutel/solve.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (ll a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define ll long long
+#define Graph vector<vector<ll>>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    ll testcases;
+    cin >> testcases;
+
+    while (testcases--) {
+        // content
+    }
+
+    return 0;
+}
