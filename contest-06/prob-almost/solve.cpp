8b3df9b450e4f5964815a6b67955637a4083372a
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-06-06 19:24:32 +0200

    feat: add solution for almost and battery

--- contest-06/prob-almost/solve.cpp
+++ contest-06/prob-almost/solve.cpp
@@ -8,16 +8,113 @@
 
 using namespace std;
 
+vector<ll> parent, noOfElements, sum;
+vector<unordered_set<ll>> elements;
+
+void initialize(ll n) {
+    parent.resize(n + 1);
+    noOfElements.resize(n + 1, 1);
+    sum.resize(n + 1);
+    elements.resize(n + 1);
+    for (ll i = 1; i <= n; ++i) {
+        parent[i] = i;
+        sum[i] = i;
+        elements[i].insert(i);
+    }
+}
+
+ll find(ll x) {
+    // utilize path compression
+    if (parent[x] != x) {
+        parent[x] = find(parent[x]);
+    }
+    return parent[x];
+}
+
+void union_sets(ll p, ll q) {
+    ll rootP = find(p);
+    ll rootQ = find(q);
+
+    if (rootP != rootQ) {
+        // make root of smaller tree point to root of larger tree
+        if (noOfElements[rootP] < noOfElements[rootQ]) {
+            swap(rootP, rootQ);
+        }
+
+        // add elements of smaller tree to larger tree
+        parent[rootQ] = rootP;
+        noOfElements[rootP] += noOfElements[rootQ];
+        sum[rootP] += sum[rootQ];
+
+        for (ll i : elements[rootQ]) {
+            elements[rootP].insert(i);
+            parent[i] = rootP;
+        }
+        elements[rootQ].clear();
+    }
+}
+
+void move(ll p, ll q) {
+    ll rootP = find(p);
+    ll rootQ = find(q);
+
+    if (rootP != rootQ) {
+        // remove element from current set
+        elements[rootP].erase(p);
+        noOfElements[rootP]--;
+        sum[rootP] -= p;
+
+        if (p == rootP) {
+            // move root element which has children
+            if (noOfElements[rootP] != 0) {
+                ll newRoot = *elements[rootP].begin();
+                noOfElements[newRoot] = 0;
+                sum[newRoot] = 0;
+                for (ll i : elements[rootP]) {
+                    parent[i] = newRoot;
+                    elements[newRoot].insert(i);
+                    noOfElements[newRoot]++;
+                    sum[newRoot] += i;
+                }
+                elements[rootP].clear();
+            }
+        }
+
+        // move element to new set
+        elements[rootQ].insert(p);
+        noOfElements[rootQ]++;
+        sum[rootQ] += p;
+        parent[p] = rootQ;
+    }
+}
+
+void query(ll p) {
+    ll rootP = find(p);
+    cout << noOfElements[rootP] << " " << sum[rootP] << endl;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    ll testcases;
-    cin >> testcases;
+    ll n, m;
+    cin >> n >> m;
+    initialize(n);
 
-    while (testcases--) {
-        // content
+    for (ll i = 0; i < m; ++i) {
+        ll type, p, q;
+        cin >> type;
+        if (type == 1) {
+            cin >> p >> q;
+            union_sets(p, q);
+        } else if (type == 2) {
+            cin >> p >> q;
+            move(p, q);
+        } else if (type == 3) {
+            cin >> p;
+            query(p);
+        }
     }
 
     return 0;
--- contest-06/prob-battery/solve.cpp
+++ contest-06/prob-battery/solve.cpp
@@ -5,8 +5,40 @@
 #define endl '\n'
 #define ll long long
 #define Graph vector<vector<ll>>
+#define WeightedGraph vector<vector<pair<ll, ll>>>
 
 using namespace std;
+
+vector<long long> dijkstraMaxWeight(ll n, WeightedGraph& adj) {
+    vector<ll> dist(n, LLONG_MAX);
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    dist[0] = 0;
+    pq.push({0, 0});
+
+    // explore all edges, starting with cheapest node
+    while (!pq.empty()) {
+        ll maxObservedDistance = pq.top().first;
+        ll currentNode = pq.top().second;
+        pq.pop();
+
+        // skip if we have already found a better path
+        if (maxObservedDistance > dist[currentNode]) continue;
+
+        // relax all edges
+        for (auto& edge : adj[currentNode]) {
+            ll neighbour = edge.first;
+            ll distanceToNeighbour = edge.second;
+
+            ll maxWeight = max(maxObservedDistance, distanceToNeighbour);
+            if (maxWeight < dist[neighbour]) {
+                dist[neighbour] = maxWeight;
+                pq.push({maxWeight, neighbour});
+            }
+        }
+    }
+
+    return dist;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -17,7 +49,24 @@ int main() {
     cin >> testcases;
 
     while (testcases--) {
-        // content
+        ll n, m;
+        cin >> n >> m;
+        WeightedGraph adj(n);
+
+        // create weighted graph
+        rep(i, m) {
+            ll a, b, w;
+            cin >> a >> b >> w;
+            adj[a].emplace_back(b, w);
+            adj[b].emplace_back(a, w);
+        }
+
+        vector<ll> dist = dijkstraMaxWeight(n, adj);
+
+        // find cost to reach 75% of cities while minimizing cost
+        sort(all(dist));
+        ll requiredCities = ceil((n - 1) * 0.75);
+        cout << dist[requiredCities] << endl;
     }
 
     return 0;
