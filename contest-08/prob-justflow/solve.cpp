c5279efde37a71e65f61373c968f7a6148b32b55
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-06-20 22:19:40 +0200

    feat: add edmonds-karp solution for justflow

--- contest-08/prob-justflow/solve.cpp
+++ contest-08/prob-justflow/solve.cpp
@@ -9,17 +9,76 @@
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.precision(10);
+// BFS to find an augmenting path and store the path in parent array
+bool bfs(ll s, ll t, vector<ll> &parent, vector<vector<ll>> &capacity, vector<vector<ll>> &adj, ll n) {
+    vector<bool> visited(n + 1, false);
+    queue<ll> q;
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
 
-    ll testcases;
-    cin >> testcases;
+    while (!q.empty()) {
+        ll u = q.front();
+        q.pop();
 
-    while (testcases--) {
-        // content
+        for (ll v : adj[u]) {
+            if (!visited[v] && capacity[u][v] > 0) {
+                q.push(v);
+                parent[v] = u;
+                visited[v] = true;
+                if (v == t) {
+                    return true;
+                }
+            }
+        }
     }
+    return false;
+}
+
+// max flow algorithm adding path flows to sum until no more augmenting paths
+ll edmondsKarp(ll s, ll t, vector<vector<ll>> &capacity, vector<vector<ll>> &adj, ll n) {
+    ll max_flow = 0;
+    vector<ll> parent(n + 1);
+
+    while (bfs(s, t, parent, capacity, adj, n)) {
+        ll path_flow = LLONG_MAX;
+        for (ll v = t; v != s; v = parent[v]) {
+            ll u = parent[v];
+            path_flow = min(path_flow, capacity[u][v]);
+        }
+
+        for (ll v = t; v != s; v = parent[v]) {
+            ll u = parent[v];
+            capacity[u][v] -= path_flow;  // forward flow
+            capacity[v][u] += path_flow;  // backward flow
+        }
+
+        max_flow += path_flow;
+    }
+
+    return max_flow;
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+
+    Graph capacity(n + 1, vector<ll>(n + 1, 0));
+    Graph adj(n + 1);
+
+    rep(i, m) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        // graph is undirected
+        capacity[a][b] += c;
+        capacity[b][a] += c;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    // find maximum flow from node 1 to node 2
+    cout << edmondsKarp(1, 2, capacity, adj, n) << endl;
 
     return 0;
 }
