aab34825276ed414f9892e942ecab36bb5120efd
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-07-04 19:47:49 +0200

    feat: add largestPrimeFactor solution for gifts

--- contest-09/prob-gifts/solve.cpp
+++ contest-09/prob-gifts/solve.cpp
@@ -9,17 +9,40 @@
 
 using namespace std;
 
+ll largestPrimeFactor(ll n) {
+    ll maxPrime = -1;
+
+    // divide n by 2 to remove all even factors
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n /= 2;
+    }
+
+    // n must be odd at this point, so we can skip even numbers
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n /= i;
+        }
+    }
+
+    // check if n is a prime number greater than 2
+    if (n > 2) {
+        maxPrime = n;
+    }
+
+    return maxPrime;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    ll testcases;
-    cin >> testcases;
+    ll n;
+    cin >> n;
 
-    while (testcases--) {
-        // content
-    }
+    cout << largestPrimeFactor(n) << endl;
 
     return 0;
 }
